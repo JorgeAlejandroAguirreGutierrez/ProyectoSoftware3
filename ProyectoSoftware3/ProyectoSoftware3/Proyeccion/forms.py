e7051615f5d62ba19ed25e57b318fad7511c3b84
DARKATOS <alejoved@yahoo.com.co>
2016-11-14 16:22:20 -0500

    cambios 14-11-5

--- ProyectoSoftware3/ProyectoSoftware3/Proyeccion/forms.py
+++ ProyectoSoftware3/ProyectoSoftware3/Proyeccion/forms.py
@@ -2,7 +2,7 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 from django import forms
-from models import InformacionDescriptiva,RecursoEstudiante,RecursoDocente
+from models import *
 class InformacionDescriptivaForm(forms.ModelForm):
     class Meta:
         model = InformacionDescriptiva
@@ -64,6 +64,72 @@ class InformacionDescriptivaForm(forms.ModelForm):
             'justificacion': forms.Textarea(attrs={'class':'form-control'}),
             'metodologia': forms.Textarea(attrs={'class':'form-control'}),
             'problema': forms.Textarea(attrs={'class':'form-control'}),
+        }
+        
+        
+class RecursoDocenteForm(forms.ModelForm):
+    class Meta:
+        model = RecursoDocente
+        exclude = ['proyecto_id']
+
+        fields = [
+            'docente_id',
+            'numero_horas_semana',
+            'fecha_inicio',
+            'fecha_final',
+            'tipo_financiacion',
+#            'proyecto_id'
+        ]
+        labels = {
+            'docente_id':'DOCENTE',
+            'numero_horas_semana':'NUMERO HORAS SEMANA',
+            'fecha_inicio':'FECHA INICIO',
+            'fecha_final':'FECHA FINAL',
+            'tipo_financiacion':'TIPO DE FINANCIACION',
+#            'proyecto_id': 'PROYECTO',
+        }
+        widgets = {
+            'docente_id': forms.Select(attrs={'class':'form-control'}),
+            'numero_horas_semana': forms.TextInput(attrs={'class':'form-control', 'type':'number'}),
+            'fecha_inicio': forms.DateInput(attrs={'class':'form-control','type':'date'}),
+            'fecha_final': forms.DateInput(attrs={'class':'form-control','type':'date'}),
+            'tipo_financiacion': forms.TextInput(attrs={'class':'form-control'}),
+#            'proyecto_id': forms.TextInput(attrs={'class':'form-control'}),
+        }
+
+class RecursoEstudianteForm(forms.ModelForm):
+    class Meta:
+        model = RecursoEstudiante
+        exclude = ['proyecto_id']
+        
+        fields = [
+            'nombre',
+            'codigo',
+            'programa_id',
+            'semestre',
+            'numero_horas_semana',
+            'fecha_inicio',
+            'fecha_final',
+#            'proyecto_id'
+        ]
+        labels = {
+            'nombre':'NOMBRE',
+            'codigo':'CODIGO',
+            'programa':'PROGRAMA',
+            'semestre':'SEMESTRE',
+            'numero_horas_semana':'NUMERO HORAS SEMANA',
+            'fecha_inicio':'FECHA INICIO',
+            'fecha_final':'FECHA FINAL',
+#            'proyecto_id': 'PROYECTO',
+        }
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class':'form-control'}),
+            'codigo': forms.TextInput(attrs={'class':'form-control'}),
+            'semestre': forms.TextInput(attrs={'class':'form-control', 'type':'number'}),
+            'numero_horas_semana': forms.TextInput(attrs={'class':'form-control', 'type':'number'}),
+            'fecha_inicio': forms.DateInput(attrs={'class':'form-control','type':'date'}),
+            'fecha_final': forms.DateInput(attrs={'class':'form-control','type':'date'}),
+#            'proyecto_id': forms.TextInput(attrs={'class':'form-control'}),
         }
     
 class ModificarRecursoEstudianteForm(forms.ModelForm):
--- ProyectoSoftware3/ProyectoSoftware3/Proyeccion/urls.py
+++ ProyectoSoftware3/ProyectoSoftware3/Proyeccion/urls.py
@@ -1,6 +1,6 @@
 
 from django.conf.urls import url
-from views import docente,cerrarSession,ConsultarProyectos,CrearProyecto, ModificarProyecto,ConsultarRecursoEstudiante,ModificarRecursoEstudiante,ConsultarRecursoDocente,ModificarRecursoDocente
+from views import *
 urlpatterns = [
     #Mapa de rutas de la aplicacion de Proyeccion 
     url(r'^Docente/$', docente),
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^CrearProyecto/$', CrearProyecto.as_view()),
     url(r'^recursoEstudiante/(?P<pk>\d+)$', ConsultarRecursoEstudiante.as_view(),name='ConsultarRecursoEstudiante'),
     url(r'^recursoDocente/(?P<pk>\d+)$', ConsultarRecursoDocente.as_view(),name='ConsultarRecursoDocente'),
+    url(r'^CrearRecursoDocente/(?P<pk>\d+)$', CrearRecursoDocente.as_view()),
+    url(r'^CrearRecursoEstudiante/(?P<pk>\d+)$', CrearRecursoEstudiante.as_view()),
     url(r'^modificarProyecto/(?P<pk>\d+)$', ModificarProyecto.as_view()),
     url(r'^modificarRecursoEstudiante/(?P<pk>\d+)$', ModificarRecursoEstudiante.as_view()),
     url(r'^modificarRecursoDocente/(?P<pk>\d+)$', ModificarRecursoDocente.as_view()),
